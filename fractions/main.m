b37f6c92bf48f500549d25b834478ef93576bbbf
Your Name <everettchurchey@crbc.edu>
2015-04-08 09:33:03 -0400

    Initial Commit

---
+++ fractions/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  fractions
+//
+//  Created by Ej Churchey on 4/8/15.
+//  Copyright (c) 2015 Ej Churchey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
