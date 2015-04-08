825323519706d719e6d2e322e16c22091ddea9c9
Your Name <everettchurchey@crbc.edu>
2015-04-08 10:54:27 -0400

    fractions

--- fractions/main.m
+++ fractions/main.m
@@ -8,10 +8,54 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Fraction: NSObject
+
+- (void) print;
+- (void) setNum: (int) n;
+- (void) setDen: (int) d;
+- (int) divide;
+
+@end
+
+@implementation Fraction
+
+{
+    int num;
+    int den;
+}
+
+- (int) divide {
+
+    return num/den;
+
+}
+
+- (void) print {
+
+    NSLog(@"%i/%i",num,den);
+}
+- (void) setNum: (int) n {num = n;}
+- (void) setDen: (int) d {den = d;}
+
+@end
+
 int main(int argc, const char * argv[]) {
+
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+        Fraction * f = [Fraction alloc];
+        
+        f = [f init];
+        
+        [f setNum:100];
+        [f setDen:10];
+        
+        [f print];
+        
+        int result = [f divide];
+        
+        NSLog(@"%i",result);
+        
     }
     return 0;
 }
